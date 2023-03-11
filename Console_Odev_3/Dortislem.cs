b85a2ceb45c3e9f3e5d204b4e3178a4d8aa8f180
m.sinans <msomercikoglu@gmail.com>
2023-03-11 17:31:57 +0300

    Add project files.

---
+++ Console_Odev_3/Dortislem.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Console_Odev_3
+{
+    internal class Dortislem
+    {
+        public int Topla(int t1, int t2)
+        {
+            int t3 = t1 + t2;
+            Console.WriteLine("Toplam: " + t3);
+            return t3;
+        }
+        public int Cikar(int t1, int t2)
+        {
+            int t3 = t1 - t2;
+            Console.WriteLine("Fark: " +  t3);
+            return t3;
+        }
+        public double Bol(double t1, double t2)
+        { 
+            double t3 = t1 / t2;
+            Console.WriteLine("Bölüm: " + t3.ToString("0.00"));
+            return t3;
+        }
+        public int Carp(int t1, int t2)
+        {
+            int t3 = t1 * t2;
+            Console.WriteLine("Çarpım: " + t3);
+            return t3;            
+        }
+        public int Mod(int t1, int t2)
+        {
+            int t3 = t1 % t2;
+            Console.WriteLine("Bölümden Kalan: " + t3);
+            return t3;
+        }
+
+    }
+}
---
+++ Console_Odev_3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Console_Odev_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dortislem di = new Dortislem();
+            int t1, t2;
+            double d1, d2;
+            Console.Write("Bir sayı girin: ");
+            t1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Bir sayı girin: ");
+            t2 = Convert.ToInt32(Console.ReadLine());
+            d1 = Convert.ToDouble(t1);
+            d2 = Convert.ToDouble(t2);
+
+            di.Topla(t1, t2);                        
+            di.Cikar(t1, t2);
+            di.Bol(d1, d2);
+            di.Carp(t1, t2);
+            di.Mod(t1, t2);
+
+            Console.Read();
+        }
+    }
+}
